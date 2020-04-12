b73ca6fc6b5a654995c1448c128d8ef8dd00b322
omarkhaled <omohamed449@yahoo.com>
2020-04-12 13:28:12 +0200

    Updates

---
+++ precache-manifest.e08c0a44da174ceef4f1329e8215abc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d39dee3dada9906719ce8d9b214a3b2",
+    "url": "/monster-okg/index.html"
+  },
+  {
+    "revision": "9ab9139cdca258520ffd",
+    "url": "/monster-okg/static/css/main.bcdc64d8.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-okg/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-okg/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ab9139cdca258520ffd",
+    "url": "/monster-okg/static/js/main.3d8dca24.chunk.js"
+  },
+  {
+    "revision": "b6d07d8359e995aefc22",
+    "url": "/monster-okg/static/js/runtime-main.3a229d17.js"
+  }
+]);
